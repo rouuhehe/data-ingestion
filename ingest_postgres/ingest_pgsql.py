6afa6189fc92281e870f8f244ea6289421084a67
rouuhehe <rojasevelyn88@gmail.com>
2025-09-30 12:13:55 -0500

    minor changes

--- ingest_postgres/ingest_pgsql.py
+++ ingest_postgres/ingest_pgsql.py
@@ -15,9 +15,9 @@ def export_postgres():
     print("pets table was exported to S3")
     s3.upload_file(ficheroUpload2, S3_BUCKET, f'proyecto-parcial/postgres/{ficheroUpload2}')
     print("statuses table was exported to S3")
-    s3.upload_file(ficheroUpload1, S3_BUCKET, f'proyecto-parcial/postgres/{ficheroUpload3}')
+    s3.upload_file(ficheroUpload3, S3_BUCKET, f'proyecto-parcial/postgres/{ficheroUpload3}')
     print("vaccines table was exported to S3")
-    s3.upload_file(ficheroUpload1, S3_BUCKET, f'proyecto-parcial/postgres/{ficheroUpload4}')
+    s3.upload_file(ficheroUpload4, S3_BUCKET, f'proyecto-parcial/postgres/{ficheroUpload4}')
     print("centers table was exported to S3")
 
     print("Postgres export completed")
